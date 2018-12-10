3f339e7613011fb924442245b4ae2c35c9b686e3
Jonathan Young <young.jpc@gmail.com>
2018-12-10 16:17:51 +1000

    Material file parsing.

--- objzero.c
+++ objzero.c
@@ -20,101 +20,119 @@
 #define OBJZ_MAX_TOKEN_LENGTH 256
 
 static char s_error[OBJZ_MAX_ERROR_LENGTH] = { 0 };
-static uint32_t s_currentLine = 1;
-static uint32_t s_currentColumn = 1;
 
-struct token_s {
+typedef struct {
+	char *buf;
+	int line, column;
+} Lexer;
+
+typedef struct {
 	char text[OBJZ_MAX_TOKEN_LENGTH];
 	int line, column;
-};
+} Token;
 
-static bool objz_isEol(const char *_buffer) {
-	OBJZ_DEBUG_ASSERT(_buffer);
-	return (*_buffer == '\n' || (_buffer[0] == '\r' && _buffer[1] != '\n'));
+static void objz_initLexer(Lexer *_lexer, char *_buf) {
+	_lexer->buf = _buf;
+	_lexer->line = _lexer->column = 1;
 }
 
-static bool objz_isEof(const char *_buffer) {
-	OBJZ_DEBUG_ASSERT(_buffer);
-	return (*_buffer == 0);
+static bool objz_isEol(const Lexer *_lexer) {
+	OBJZ_DEBUG_ASSERT(_lexer);
+	return (_lexer->buf[0] == '\n' || (_lexer->buf[0] == '\r' && _lexer->buf[1] != '\n'));
 }
 
-static bool objz_isWhitespace(const char *_buffer) {
-	OBJZ_DEBUG_ASSERT(_buffer);
-	return (*_buffer == ' ' || *_buffer == '\t' || *_buffer == '\r');
+static bool objz_isEof(const Lexer *_lexer) {
+	OBJZ_DEBUG_ASSERT(_lexer);
+	return (_lexer->buf[0] == 0);
 }
 
-static const char *objz_skipLine(const char *_buffer) {
-	OBJZ_DEBUG_ASSERT(_buffer);
+static bool objz_isWhitespace(const Lexer *_lexer) {
+	OBJZ_DEBUG_ASSERT(_lexer);
+	return (_lexer->buf[0] == ' ' || _lexer->buf[0] == '\t' || _lexer->buf[0] == '\r');
+}
+
+static void objz_skipLine(Lexer *_lexer) {
+	OBJZ_DEBUG_ASSERT(_lexer);
 	for (;;) {
-		if (objz_isEof(_buffer))
+		if (objz_isEof(_lexer))
 			break;
-		if (objz_isEol(_buffer)) {
-			s_currentColumn = 1;
-			s_currentLine++;
-			return _buffer + 1;
+		if (objz_isEol(_lexer)) {
+			_lexer->column = 1;
+			_lexer->line++;
+			_lexer->buf++;
+			break;
 		}
-		_buffer++;
-		s_currentColumn++;
+		_lexer->buf++;
+		_lexer->column++;
 	}
-	return _buffer;
 }
 
-static const char *objz_skipWhitespace(const char *_buffer) {
-	OBJZ_DEBUG_ASSERT(_buffer);
+static void objz_skipWhitespace(Lexer *_lexer) {
+	OBJZ_DEBUG_ASSERT(_lexer);
 	for (;;) {
-		if (objz_isEof(_buffer))
+		if (objz_isEof(_lexer))
 			break;
-		if (!objz_isWhitespace(_buffer))
+		if (!objz_isWhitespace(_lexer))
 			break;
-		_buffer++;
-		s_currentColumn++;
+		_lexer->buf++;
+		_lexer->column++;
 	}
-	return _buffer;
 }
 
-static const char *objz_parseToken(const char *_buffer, struct token_s *_token) {
-	OBJZ_DEBUG_ASSERT(_buffer);
+static void objz_tokenize(Lexer *_lexer, Token *_token, bool includeWhitespace) {
+	OBJZ_DEBUG_ASSERT(_lexer);
 	OBJZ_DEBUG_ASSERT(_token);
 	int i = 0;
-	_buffer = objz_skipWhitespace(_buffer);
-	_token->line = s_currentLine;
-	_token->column = s_currentColumn;
+	objz_skipWhitespace(_lexer);
+	_token->line = _lexer->line;
+	_token->column = _lexer->column;
 	for (;;) {
-		if (objz_isEof(_buffer) || objz_isEol(_buffer) || objz_isWhitespace(_buffer))
+		if (objz_isEof(_lexer) || objz_isEol(_lexer) || (!includeWhitespace && objz_isWhitespace(_lexer)))
 			break;
-		_token->text[i++] = *_buffer;
-		_buffer++;
-		s_currentColumn++;
+		_token->text[i++] = _lexer->buf[0];
+		_lexer->buf++;
+		_lexer->column++;
 	}
 	_token->text[i] = 0;
-	return _buffer;
 }
 
-static const char *objz_parseFloatArray(const char *_buffer, float *_result, int n) {
-	OBJZ_DEBUG_ASSERT(_buffer);
+static bool objz_parseFloats(Lexer *_lexer, float *_result, int n) {
+	OBJZ_DEBUG_ASSERT(_lexer);
 	OBJZ_DEBUG_ASSERT(_result);
-	OBJZ_DEBUG_ASSERT(n > 0);
+	Token token;
 	for (int i = 0; i < n; i++) {
-		struct token_s token;
-		_buffer = objz_parseToken(_buffer, &token);
+		objz_tokenize(_lexer, &token, false);
 		if (strlen(token.text) == 0) {
-			snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Error parsing float array", token.line, token.column);
-			return NULL;
+			snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Error parsing float", token.line, token.column);
+			return false;
 		}
 		_result[i] = (float)atof(token.text);
 	}
-	return _buffer;
+	return true;
 }
 
-static const char *objz_parseFace(const char *_buffer, int *_face, int *_n) {
-	OBJZ_DEBUG_ASSERT(_buffer);
+static bool objz_parseInt(Lexer *_lexer, int *_result) {
+	OBJZ_DEBUG_ASSERT(_lexer);
+	OBJZ_DEBUG_ASSERT(_result);
+	Token token;
+	objz_tokenize(_lexer, &token, false);
+	if (strlen(token.text) == 0) {
+		snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Error parsing int", token.line, token.column);
+		return false;
+	}
+	*_result = atoi(token.text);
+	return true;
+}
+
+static bool objz_parseFace(Lexer *_lexer, int *_face, int *_n) {
+	OBJZ_DEBUG_ASSERT(_lexer);
 	OBJZ_DEBUG_ASSERT(_face);
-	struct token_s token;
+	Token token;
 	*_n = 0;
 	for (int i = 0; i < 4; i++) {
-		_buffer = objz_parseToken(_buffer, &token);
+		objz_tokenize(_lexer, &token, false);
 		if (token.text[0] == 0) {
-			if (i == 3 && objz_isEol(_buffer))
+			if (i == 3 && objz_isEol(_lexer))
 				break;
 			goto error;
 		}
@@ -126,95 +144,219 @@ static const char *objz_parseFace(const char *_buffer, int *_face, int *_n) {
 		_face[i * 3 + 0] = atoi(triplet);
 		// vt (optional)
 		triplet = strtok(NULL, delim);
-		_face[i * 3 + 1] = triplet ? atoi(triplet) : -1;
+		_face[i * 3 + 1] = triplet ? atoi(triplet) : INT_MAX;
 		// vn (optional)
 		triplet = strtok(NULL, delim);
-		_face[i * 3 + 2] = triplet ? atoi(triplet) : -1;
+		_face[i * 3 + 2] = triplet ? atoi(triplet) : INT_MAX;
 		(*_n)++;
 	}
-	return _buffer;
+	return true;
 error:
 	snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Failed to parse face", token.line, token.column);
-	return NULL;
+	return false;
 }
 
-int objz_load(const char *_filename) {
-	int result = -1;
+static char *objz_readFile(const char *_filename) {
 	FILE *f = fopen(_filename, "rb");
 	if (!f) {
 		snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "Failed to open file '%s'", _filename);
-		return result;
+		return NULL;
 	}
 	fseek(f, 0, SEEK_END);
 	const size_t fileLength = (size_t)ftell(f);
 	fseek(f, 0, SEEK_SET);
 	if (fileLength <= 0) {
 		fclose(f);
-		return result;
+		return NULL;
 	}
 	char *buffer = malloc(fileLength + 1);
 	const size_t bytesRead = fread(buffer, 1, fileLength, f);
-	if (bytesRead < fileLength)
-		goto cleanup;
+	fclose(f);
+	if (bytesRead < fileLength) {
+		free(buffer);
+		return NULL;
+	}
 	buffer[fileLength] = 0;
-	const char *current = buffer;
-	struct token_s token;
-	for (;;) {
-		current = objz_parseToken(current, &token);
-		if (token.text[0] == 0) {
-			if (objz_isEof(current))
+	return buffer;
+}
+
+#define OBJZ_MAT_TOKEN_STRING 0
+#define OBJZ_MAT_TOKEN_FLOAT  1
+#define OBJZ_MAT_TOKEN_INT    2
+
+typedef struct {
+	const char *name;
+	int type;
+	size_t offset;
+	int n;
+} MaterialTokenDef;
+
+static MaterialTokenDef s_materialTokens[] = {
+	{ "d", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, d), 1 },
+	{ "illum", OBJZ_MAT_TOKEN_INT, offsetof(Material, illum), 1 },
+	{ "Ka", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, Ka), 3 },
+	{ "Kd", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, Kd), 3 },
+	{ "Ke", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, Ke), 3 },
+	{ "Ks", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, Ks), 3 },
+	{ "Ni", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, Ni), 1 },
+	{ "Ns", OBJZ_MAT_TOKEN_FLOAT, offsetof(Material, Ns), 1 },
+	{ "map_Bump", OBJZ_MAT_TOKEN_STRING, offsetof(Material, map_Bump), 1 },
+	{ "map_Ka", OBJZ_MAT_TOKEN_STRING, offsetof(Material, map_Ka), 1 },
+	{ "map_Kd", OBJZ_MAT_TOKEN_STRING, offsetof(Material, map_Kd), 1 }
+};
+
+static void objz_printMaterial(const Material *_mat) {
+	printf("Material %s\n", _mat->name);
+	printf("   d: %g\n", _mat->d);
+	printf("   illum: %d\n", _mat->illum);
+	printf("   Ka: %g %g %g\n", _mat->Ka[0], _mat->Ka[1], _mat->Ka[2]);
+	printf("   Kd: %g %g %g\n", _mat->Kd[0], _mat->Kd[1], _mat->Kd[2]);
+	printf("   Ke: %g %g %g\n", _mat->Ke[0], _mat->Ke[1], _mat->Ke[2]);
+	printf("   Ks: %g %g %g\n", _mat->Ks[0], _mat->Ks[1], _mat->Ks[2]);
+	printf("   Ni: %g\n", _mat->Ni);
+	printf("   Ns: %g\n", _mat->Ns);
+	printf("   map_Bump: %s\n", _mat->map_Bump);
+	printf("   map_Ka: %s\n", _mat->map_Ka);
+	printf("   map_Kd: %s\n", _mat->map_Kd);
+}
+
+static void objz_initMaterial(Material *_mat) {
+	memset(_mat, 0, sizeof(*_mat));
+}
+
+static int objz_loadMaterial(const char *_objFilename, const char *_materialName) {
+	char filename[256] = { 0 };
+	const char *lastSlash = strrchr(_objFilename, '/');
+	if (!lastSlash)
+		lastSlash = strrchr(_objFilename, '\\');
+	if (lastSlash) {
+		for (int i = 0;; i++) {
+			filename[i] = _objFilename[i];
+			if (&_objFilename[i] == lastSlash)
 				break;
-		} else {
-			if (token.text[0] == '#')
-				current = objz_skipLine(current);
-			else if (OBJZ_STRICMP(token.text, "f") == 0) {
-				int face[4*3], numVerts;
-				current = objz_parseFace(current, face, &numVerts);
-				if (!current)
-					goto cleanup;
-				current = objz_skipLine(current);
-				printf("Face (%d verts):\n", numVerts);
-				for (int i = 0; i < numVerts; i++)
-					printf("%d %d %d\n", face[i * 3 + 0], face[i * 3 + 1], face[i * 3 + 2]);
-			}
-			else if (OBJZ_STRICMP(token.text, "o") == 0) {
-				current = objz_parseToken(current, &token);
-				if (token.text[0] == 0) {
-					snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Failed to parse object name", token.line, token.column);
-					goto cleanup;
-				}
-				current = objz_skipLine(current);
-				printf("Object: %s\n", token.text);
-			}
-			else if (OBJZ_STRICMP(token.text, "s") == 0) {
-				current = objz_skipLine(current); // Ignore smoothing groups
-			}
-			else if (OBJZ_STRICMP(token.text, "v") == 0 || OBJZ_STRICMP(token.text, "vn") == 0 || OBJZ_STRICMP(token.text, "vt") == 0) {
-				int n = 3;
-				if (OBJZ_STRICMP(token.text, "vt") == 0)
-					n = 2;
-				float vertex[3];
-				current = objz_parseFloatArray(current, vertex, n);
-				if (!current)
-					goto cleanup;
-				current = objz_skipLine(current);
-				if (OBJZ_STRICMP(token.text, "v") == 0)
-					printf("Vertex: %g %g %g\n", vertex[0], vertex[1], vertex[2]);
-				else if (OBJZ_STRICMP(token.text, "vn") == 0)
-					printf("Normal: %g %g %g\n", vertex[0], vertex[1], vertex[2]);
-				else if (OBJZ_STRICMP(token.text, "vt") == 0)
-					printf("Texcoord: %g %g\n", vertex[0], vertex[1]);
-			}
-			else {
-				snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Unknown token '%s'", token.line, token.column, token.text);
+		}
+		strncat(filename, _materialName, sizeof(filename));
+	} else
+		strncpy(filename, _materialName, sizeof(filename));
+	int result = -1;
+	char *buffer = objz_readFile(filename);
+	if (!buffer)
+		goto cleanup;
+	Lexer lexer;
+	objz_initLexer(&lexer, buffer);
+	Token token;
+	Material mat;
+	objz_initMaterial(&mat);
+	for (;;) {
+		objz_tokenize(&lexer, &token, false);
+		if (token.text[0] == 0) {
+			if (objz_isEof(&lexer))
+				break;
+		} else if (OBJZ_STRICMP(token.text, "newmtl") == 0) {
+			objz_tokenize(&lexer, &token, false);
+			if (token.text[0] == 0) {
+				snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Expected name after 'newmtl'", token.line, token.column);
 				goto cleanup;
 			}
+			if (mat.name[0] != 0)
+				objz_printMaterial(&mat);
+			objz_initMaterial(&mat);
+			strncpy(mat.name, token.text, OBJZ_NAME_MAX);
+		} else {
+			for (int i = 0; i < sizeof(s_materialTokens) / sizeof(s_materialTokens[0]); i++) {
+				const MaterialTokenDef *mtd = &s_materialTokens[i];
+				uint8_t *dest = &((uint8_t *)&mat)[mtd->offset];
+				if (OBJZ_STRICMP(token.text, mtd->name) == 0) {
+					if (mtd->type == OBJZ_MAT_TOKEN_STRING) {
+						objz_tokenize(&lexer, &token, false);
+						if (token.text[0] == 0) {
+							snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Expected name after '%s'", token.line, token.column, mtd->name);
+							goto cleanup;
+						}
+						strncpy((char *)dest, token.text, OBJZ_NAME_MAX);
+					} else if (mtd->type == OBJZ_MAT_TOKEN_FLOAT) {
+						if (!objz_parseFloats(&lexer, (float *)dest, mtd->n))
+							goto cleanup;
+					} else if (mtd->type == OBJZ_MAT_TOKEN_INT) {
+						if (!objz_parseInt(&lexer, (int *)dest))
+							goto cleanup;
+					}
+					break;
+				}
+			}
 		}
+		objz_skipLine(&lexer);
+	}
+	if (mat.name[0] != 0)
+		objz_printMaterial(&mat);
+	result = 1;
+cleanup:
+	free(buffer);
+	return result;
+}
+
+int objz_load(const char *_filename) {
+	int result = -1;
+	char *buffer = objz_readFile(_filename);
+	if (!buffer)
+		goto cleanup;
+	Lexer lexer;
+	objz_initLexer(&lexer, buffer);
+	Token token;
+	for (;;) {
+		objz_tokenize(&lexer, &token, false);
+		if (token.text[0] == 0) {
+			if (objz_isEof(&lexer))
+				break;
+		} else if (OBJZ_STRICMP(token.text, "f") == 0) {
+			int face[4*3], numVerts;
+			if (!objz_parseFace(&lexer, face, &numVerts))
+				goto cleanup;
+			/*printf("Face (%d verts):\n", numVerts);
+			for (int i = 0; i < numVerts; i++)
+				printf("%d %d %d\n", face[i * 3 + 0], face[i * 3 + 1], face[i * 3 + 2]);*/
+		} else if (OBJZ_STRICMP(token.text, "mtllib") == 0) {
+			objz_tokenize(&lexer, &token, true);
+			if (token.text[0] == 0) {
+				snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Expected name after 'mtllib'", token.line, token.column);
+				goto cleanup;
+			}
+			printf("Material lib: %s\n", token.text);
+			if (!objz_loadMaterial(_filename, token.text))
+				goto cleanup;
+		} else if (OBJZ_STRICMP(token.text, "o") == 0) {
+			objz_tokenize(&lexer, &token, false);
+			if (token.text[0] == 0) {
+				snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Expected name after 'o'", token.line, token.column);
+				goto cleanup;
+			}
+			printf("Object: %s\n", token.text);
+		} else if (OBJZ_STRICMP(token.text, "usemtl") == 0) {
+			objz_tokenize(&lexer, &token, false);
+			if (token.text[0] == 0) {
+				snprintf(s_error, OBJZ_MAX_ERROR_LENGTH, "(%u:%u) Expected name after 'usemtl'", token.line, token.column);
+				goto cleanup;
+			}
+			printf("Use material: %s\n", token.text);
+		} else if (OBJZ_STRICMP(token.text, "v") == 0 || OBJZ_STRICMP(token.text, "vn") == 0 || OBJZ_STRICMP(token.text, "vt") == 0) {
+			int n = 3;
+			if (OBJZ_STRICMP(token.text, "vt") == 0)
+				n = 2;
+			float vertex[3];
+			if (!objz_parseFloats(&lexer, vertex, 2))
+				goto cleanup;
+			/*if (OBJZ_STRICMP(token.text, "v") == 0)
+				printf("Vertex: %g %g %g\n", vertex[0], vertex[1], vertex[2]);
+			else if (OBJZ_STRICMP(token.text, "vn") == 0)
+				printf("Normal: %g %g %g\n", vertex[0], vertex[1], vertex[2]);
+			else if (OBJZ_STRICMP(token.text, "vt") == 0)
+				printf("Texcoord: %g %g\n", vertex[0], vertex[1]);*/
+		}
+		objz_skipLine(&lexer);
 	}
 	result = 1;
 cleanup:
 	free(buffer);
-	fclose(f);
 	return result;
 }
 
--- objzero.h
+++ objzero.h
@@ -7,6 +7,25 @@
 extern "C" {
 #endif
 
+#ifndef OBJZ_NAME_MAX
+#define OBJZ_NAME_MAX 64
+#endif OBJZ_NAME_MAX
+
+typedef struct {
+	char name[OBJZ_NAME_MAX];
+	float d;
+	int illum;
+	float Ka[3];
+	float Kd[3];
+	float Ke[3];
+	float Ks[3];
+	float Ni;
+	float Ns;
+	char map_Bump[OBJZ_NAME_MAX];
+	char map_Ka[OBJZ_NAME_MAX];
+	char map_Kd[OBJZ_NAME_MAX];
+} Material;
+
 int objz_load(const char *_filename);
 const char *objz_getError();
 
